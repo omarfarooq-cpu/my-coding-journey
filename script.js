841ed60597134163d15539fbd8c45a5255741cf5
omarfarooq-cpu <omarfarooqf2345@gmail.com>
2026-01-11 17:20:46 +0530

    Day 5 : Prepared portfolio for deployment

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-const button = document.getElementById("helloBtn");
+const button = document.getElementById("Connect with me");
 const message = document.getElementById("message");
 
 button.addEventListener("click",()=>{
+    if(message.textContent===""){
     message.textContent = "Thanks for visiting my coding journey!";
     message.style.marginTop= "20px";
     message.style.fontSize= "18px";
+}else{
+    message.textContent = "";
+}
 });
