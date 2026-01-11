aa3a6b7345659f54238cf85ee3454e09df4ef9c5
omarfarooq-cpu <omarfarooqf2345@gmail.com>
2026-01-11 16:49:14 +0530

    Day-4 updated button with intraction using javascript

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-function sayHello(){
-    alert("Welcome this is my coding journey!");
-}
+const button = document.getElementById("helloBtn");
+const message = document.getElementById("message");
+
+button.addEventListener("click",()=>{
+    message.textContent = "Thanks for visiting my coding journey!";
+    message.style.marginTop= "20px";
+    message.style.fontSize= "18px";
+});
